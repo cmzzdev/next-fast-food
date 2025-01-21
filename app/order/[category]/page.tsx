325377a2063934f7e85b0131c6707ba480df63d4
Carles <carmar85@gmail.com>
2025-01-21 18:17:05 +0100

    components produts, ui, utils, media and order page WIP

---
+++ app/order/[category]/page.tsx
@@ -0,0 +1,32 @@
+import ProductCard from "@/components/product/ProductCard";
+import { prisma } from "@/src/lib/prisma";
+
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
+  return products;
+}
+
+export default async function OrderPage({
+  params,
+}: {
+  params: { category: string };
+}) {
+  const { category } = await params;
+  const products = await getProducts(category);
+  return (
+    <>
+      <h1 className="text-2xl my-10">Choose your products</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
+}
