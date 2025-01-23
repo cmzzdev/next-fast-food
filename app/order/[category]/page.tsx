f857c62056030f041750e007eab8f85113d407c7
Carles <carmar85@gmail.com>
2025-01-23 13:02:50 +0100

    admin panel, complete order action, orderCard, Heading Logo

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "@/components/product/ProductCard";
+import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
 async function getProducts(category: string) {
@@ -21,7 +22,7 @@ export default async function OrderPage({
   const products = await getProducts(category);
   return (
     <>
-      <h1 className="text-2xl my-10">Choose your products</h1>
+      <Heading>Choose your products</Heading>
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
---
+++ components/ui/Heading.tsx
@@ -0,0 +1,3 @@
+export default function Heading({ children }: { children: React.ReactNode }) {
+  return <div className="text-2xl my-10">{children}</div>;
+}
